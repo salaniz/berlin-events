afa0b10dc10e6cfb4a9d086c9558491df5ebd30d
Steeex <stephan.alaniz@gmail.com>
2015-07-12 14:08:18 +0200

    some desgin/theming experiments

--- webapp/app/map/map.js
+++ webapp/app/map/map.js
@@ -148,7 +148,7 @@ angular.module('berlinerSchulenApp')
 						layer: 'schools'
 					};
 
-					marker.icon = $scope.icons.bluegrey_icon
+					marker.icon = $scope.icons.red_icon
 					/*
 					//choose the icon depending on schooltype
 					switch (schools[i].Schulart) {
